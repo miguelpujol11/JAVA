18806ba5371c4c3ed914d809dac12f95b5a60abb
miguelpujol11 <miguelpujol905@gmail.com>
2024-10-14 13:08:03 +0200

    Add files via upload

---
+++ Comprobacion de edad/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in);
+System.out.println("Introduce tu edad");
+int edad = in.nextInt();
+
+if(edad >= 18) {
+	
+System.out.println("Eres mayor de edad");	
+
+}else{
+	System.out.println("No eres mayor de edad");
+	
+	
+	
+}
+
+
+
+
+
+}
+
+}
+			
